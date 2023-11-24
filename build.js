a36bd997c34b1522d86984b5042d752b9e32361c
teznote <teznote@outlook.com>
2023-11-25 08:31:29 +0900

    update2

--- build.js
+++ build.js
@@ -77,7 +77,7 @@ function build_md() {
       content = minify(content, { collapseWhitespace: true })
       const tar_json = './docs' + (pathname === '/' ? '/index' : pathname) + '.json'
       fs.outputJSONSync(tar_json, { pathname, ver, content })
-      pageinfo_new.push({ pathname, ...page, tar_json, src_md })
+      pageinfo_new.push({ pathname, ver, ...page, tar_json, src_md })
     } else {
       pageinfo_new.push(exist_md)
     }
