e28ce264e96a57f41c0f160ce3835ba6728fbec2
teznote <98962783+teznote@users.noreply.github.com>
2024-01-05 08:36:13 +0900

    updated font

--- uno.config.ts
+++ uno.config.ts
@@ -33,8 +33,8 @@ export default defineConfig({
     //   // ...
     // },
     fontFamily: {
-      sans: 'NeoDunggeunmo,monospace',//'-apple-system,BlinkMacSystemFont,"Segoe UI","Noto Sans",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
-      mono: 'NeoDunggeunmo,monospace',//'ui-monospace,SFMono-Regular,"SF Mono",Menlo,Consolas,"Liberation Mono",monospace',
+      sans: 'Hack,NanumSquareNeo,NeoDunggeunmo,monospace',//'-apple-system,BlinkMacSystemFont,"Segoe UI","Noto Sans",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
+      mono: 'NanumSquareNeo,NeoDunggeunmo,monospace',//'ui-monospace,SFMono-Regular,"SF Mono",Menlo,Consolas,"Liberation Mono",monospace',
     },
   },
   presets: [
